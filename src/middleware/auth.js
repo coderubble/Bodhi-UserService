233a9c6361d208aaf0939590b63d7a18de0e2734
lakshmi/achuth <coderubble@gmail.com>
2020-04-12 23:12:38 +1000

    extenalization of dev.env and no auth dev mode

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,13 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-  const token = req.headers["x-access-token"] || req.headers["authorization"];
-  if (!token) return res.status(401).send("Access denied. No token provided.");
-  try {    
-    const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
-    req.email_id = decoded;
-    next();
-  } catch (ex) {
-    res.status(400).send("Invalid token.");
+  if (!process.env.DEV) {
+    const token = req.headers["x-access-token"] || req.headers["authorization"];
+    if (!token) return res.status(401).send("Access denied. No token provided.");
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+      req.email_id = decoded;
+      next();
+    } catch (ex) {
+      res.status(400).send("Invalid token.");
+    }
   }
 };
